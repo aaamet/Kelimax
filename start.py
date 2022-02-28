f6e792bab21e4e8409b788e07aca2bf944681836
Ahmet <97486611+aaamet@users.noreply.github.com>
2022-02-28 19:40:41 +0000

    Updated

--- start.py
+++ start.py
@@ -1,17 +1,17 @@
 import commands
-import generator
 
 i = 0
 
 commands.intro()
-
 commands.options()
 
 while i < 3:
     choice = int(input(" > "))
     if choice == 1:
-        generator.start_generator()
+        commands.start_generator()
     if choice == 2:
+        commands.settings()
+    if choice == 3:
         commands.tell_about()
         input("Press Enter to go back to the main menu...")
         commands.terminal_clear()
